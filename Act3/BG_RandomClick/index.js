d2749cb3c6eeab6697e1b3d3559c81cb961aa552
Dashiell24 <dashanog7@gmail.com>
2025-03-27 11:40:47 +0800

    Delete Act3/BG_RandomClick directory

--- Act3/BG_RandomClick/index.js
+++
@@ -1,6 +0,0 @@
-function RandomColor() {
-    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, 
-                             ${Math.floor(Math.random() * 256)}, 
-                             ${Math.floor(Math.random() * 256)})`;
-    document.body.style.backgroundColor = randomColor;
-}
